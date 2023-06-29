bc03d6affa96e2feae6f59fe271cad26d8518af5
PRINCE <94777825+Prince-Codemon@users.noreply.github.com>
2023-06-29 23:44:03 +0530

    fix time

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,5 +1,5 @@
 const cookingTime = (time) => {
-  if (time < 60) {
+  if (time <= 60) {
     return `${time} min`;
   } else if (time > 60) {
     return `${Math.floor(time / 60)} hr ${time % 60} min`;
